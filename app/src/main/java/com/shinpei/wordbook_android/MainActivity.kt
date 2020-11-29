b347d53c6a5058fbfb121c86d9759b06d9a481ae
p3ys1n <diggy_world@yahoo.co.jp>
2020-11-29 23:28:38 +0900

    create home activity (almost empty)

--- app/src/main/java/com/shinpei/wordbook_android/MainActivity.kt
+++ app/src/main/java/com/shinpei/wordbook_android/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.shinpei.wordbook_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shinpei.wordbook_android.activities.auth.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener{ moveToLoginView() }
+    }
+
+    private fun moveToLoginView() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/shinpei/wordbook_android/activities/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.shinpei.wordbook_android.activities.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shinpei.wordbook_android.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
